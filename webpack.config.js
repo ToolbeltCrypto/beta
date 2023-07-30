3e7cd13fd9344b492eea7181b8cce89282b12fd8
ToolbeltCrypto <137102380+ToolbeltCrypto@users.noreply.github.com>
2023-07-30 15:22:58 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '',
-    filename: 'main.js'
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '',
+        filename: 'main.js'
     },
-    mode: 'production'
 };
